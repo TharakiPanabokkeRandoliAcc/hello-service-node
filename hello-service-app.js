c8551209d27db83f973cba018fec9539983682d1
TharakiPanabokkeRandoliAcc <tharaki.panabokke@randoli.ca>
2021-02-25 16:39:28 +0530

    msg

--- hello-service-app.js
+++ hello-service-app.js
@@ -2,12 +2,18 @@ var http = require("http");
 
 http.createServer(function (request, response) {
 
-    if (request.url === '/hello')
+    if (request.url === '/')
     {
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.end('Hello! This is Hello-service (Node)\n');
     }
 
-}).listen(8081);
+    if (request.url === '/hello')
+    {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Hello! This is a route in Hello-service (Node)\n');
+    }
 
-console.log('Server running at http://127.0.0.1:8081/');
+}).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080/');
